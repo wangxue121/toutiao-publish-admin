72f334a9bc2f2ce8f6d54c7c642f2c1cf167968a
wangxue <2394512956@qq.com>
2020-05-07 18:39:27 +0800

    首页背景图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -20,4 +20,10 @@ export default {
 
 </script>
 <style scoped lang='less'>
+.home-container {
+  width: 100%;
+  height: 100%;
+  background: url('./首页背景图.png') no-repeat;
+  background-size: cover;
+}
 </style>
